58de3abf34beeafb5fe24533c66b1353b3aa9b01
Danny Lin <danny.l@17.media>
2018-03-15 11:58:48 +0800

    Add "+" menu item

--- app/src/main/java/com/dl/dynamicexoplayer/MainActivity.java
+++ app/src/main/java/com/dl/dynamicexoplayer/MainActivity.java
@@ -2,6 +2,9 @@ package com.dl.dynamicexoplayer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,25 @@ public class MainActivity extends AppCompatActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.main, menu);
+
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+			case R.id.menu_item_main_add_media:
+				Toast.makeText(this, "Add media", Toast.LENGTH_SHORT).show();
+
+				return true;
+
+			default:
+				return super.onOptionsItemSelected(item);
+		}
 	}
 }
